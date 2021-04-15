fd92558440cf1b803f2c445670d03c8f9e79cfa5
Eduardo Rosas <lalorosas@outlook.com>
2021-04-15 15:24:05 -0500

    New App constructor

--- TravelRecordApp/App.xaml.cs
+++ TravelRecordApp/App.xaml.cs
@@ -6,11 +6,22 @@ namespace TravelRecordApp
 {
     public partial class App : Application
     {
+        public static string databaseLocation = string.Empty;
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databasePath)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            databaseLocation = databasePath;
         }
 
         protected override void OnStart()
