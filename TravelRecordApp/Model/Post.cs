93691170c1056c19a04f10e8bc14a29713753b51
Eduardo Rosas <lalorosas@outlook.com>
2021-04-15 15:33:11 -0500

    The Post class

---
+++ TravelRecordApp/Model/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SQLite;
+
+namespace TravelRecordApp.Model
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Experience { get; set; }
+    }
+}
