66a32b7cd4df374d72aff14f5b78ea076c1aa4d9
Eduardo Rosas <lalorosas@outlook.com>
2021-04-15 15:36:02 -0500

    Add SQLite attributes

--- TravelRecordApp/Model/Post.cs
+++ TravelRecordApp/Model/Post.cs
@@ -5,7 +5,10 @@ namespace TravelRecordApp.Model
 {
     public class Post
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
+        [MaxLength(250)]
         public string Experience { get; set; }
     }
 }
