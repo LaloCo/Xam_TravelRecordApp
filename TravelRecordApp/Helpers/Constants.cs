06217502e7c774b703015905d5bb3d11da79c328
Eduardo Rosas <lalorosas@outlook.com>
2021-05-04 18:59:44 -0500

    Create request URL

---
+++ TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TravelRecordApp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "CZPYHQC5IXEWAQ2SLCOYMZII11DVHXOPAZKDVQAC1M1YWISC";
+        public const string CLIENT_SECRET = "1QWSOQQLNVNWEVTFK0XFDEI5GFCNM5KIDO4VXXZC5DSCW4MC";
+    }
+}
---
+++ TravelRecordApp/Model/Venue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TravelRecordApp.Helpers;
+
+namespace TravelRecordApp.Model
+{
+    public class Venue
+    {
+        public static string GenerateUrl(double lat, double lon)
+        {
+            return string.Format(Constants.VENUE_SEARCH, lat, lon, Constants.CLIENT_ID, Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
