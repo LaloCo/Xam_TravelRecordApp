05960865769bf27f387ffc7c67ae7fec5c4205ac
Eduardo Rosas <lalorosas@outlook.com>
2021-05-04 17:01:16 -0500

    Center map and handle location changes

--- TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/MapPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Plugin.Geolocator;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace TravelRecordApp
 {
@@ -28,8 +30,27 @@ namespace TravelRecordApp
             {
                 var location = await Geolocation.GetLocationAsync();
 
+                var locator = CrossGeolocator.Current;
+                locator.PositionChanged += Locator_PositionChanged;
+                await locator.StartListeningAsync(new TimeSpan(0,1,0),100);
+
                 locationsMap.IsShowingUser = true;
+
+                CenterMap(location.Latitude, location.Longitude);
             }
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            CenterMap(e.Position.Latitude, e.Position.Longitude);
+        }
+
+        private void CenterMap(double latitude, double longitude)
+        {
+            Position center = new Position(latitude, longitude);
+            MapSpan span = new MapSpan(center, 1, 1);
+
+            locationsMap.MoveToRegion(span);
         }
 
         private async Task<PermissionStatus> CheckAndRequestLocationPermission()
