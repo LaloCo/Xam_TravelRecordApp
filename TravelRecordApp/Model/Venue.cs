16ab4d606ffa315bda12f7c3cc484f5e7ae2cd6a
Eduardo Rosas <lalorosas@outlook.com>
2021-05-06 22:01:02 -0500

    Venue model

--- TravelRecordApp/Model/Venue.cs
+++ TravelRecordApp/Model/Venue.cs
@@ -1,10 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 using TravelRecordApp.Helpers;
 
 namespace TravelRecordApp.Model
 {
+    public class Location
+    {
+        public string address { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public int distance { get; set; }
+        public string postalCode { get; set; }
+        public string cc { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public IList<string> formattedAddress { get; set; }
+        public string crossStreet { get; set; }
+    }
+
+    public class Icon
+    {
+        public string prefix { get; set; }
+        public string suffix { get; set; }
+    }
+
+    public class Category
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string pluralName { get; set; }
+        public string shortName { get; set; }
+        public Icon icon { get; set; }
+        public bool primary { get; set; }
+    }
+
     public class Venue
     {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Location location { get; set; }
+        public IList<Category> categories { get; set; }
+    }
+
+    public class Response
+    {
+        public IList<Venue> venues { get; set; }
+    }
+
+    public class VenueRoot
+    {
+        public Response response { get; set; }
+
         public static string GenerateUrl(double lat, double lon)
         {
             return string.Format(Constants.VENUE_SEARCH, lat, lon, Constants.CLIENT_ID, Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
