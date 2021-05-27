b20f66661d93993e0516c12e33e256a59eb5c772
Eduardo Rosas <lalorosas@outlook.com>
2021-05-27 18:45:59 -0500

    Android implementation

--- TravelRecordApp.Android/Dependencies/Auth.cs
+++ TravelRecordApp.Android/Dependencies/Auth.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Firebase.Auth;
 using TravelRecordApp.Helpers;
 using Xamarin.Forms;
 
@@ -14,22 +15,64 @@ namespace TravelRecordApp.Droid.Dependencies
 
         public string GetCurrentUserId()
         {
-            throw new NotImplementedException();
+            return FirebaseAuth.Instance.CurrentUser.Uid;
         }
 
         public bool IsAuthenticated()
         {
-            throw new NotImplementedException();
+            return FirebaseAuth.Instance.CurrentUser != null;
         }
 
         public async Task<bool> LoginUser(string email, string password)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+
+                return true;
+            }
+            catch(FirebaseAuthWeakPasswordException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidCredentialsException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidUserException ex)
+            {
+                throw new Exception("There is no user record corresponding to this identifier");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an unknown error.");
+            }
         }
 
         public async Task<bool> RegisterUser(string email, string password)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+
+                return true;
+            }
+            catch (FirebaseAuthWeakPasswordException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidCredentialsException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthUserCollisionException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was an unknown error.");
+            }
         }
     }
 }
