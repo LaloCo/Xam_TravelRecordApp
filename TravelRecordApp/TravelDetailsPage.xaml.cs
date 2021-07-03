95e7c9b8ae1ee55a138f6cba5ab42632171e4112
Eduardo Rosas <lalorosas@outlook.com>
2021-07-03 12:13:20 -0500

    simpler command implementation

--- TravelRecordApp/TravelDetailsPage.xaml.cs
+++ TravelRecordApp/TravelDetailsPage.xaml.cs
@@ -17,40 +17,5 @@ namespace TravelRecordApp
             _selectedPost = selectedPost;
             experienceEntry.Text = selectedPost.Experience;
         }
-
-        async void Delete_Clicked(System.Object sender, System.EventArgs e)
-        {
-            //using (SQLiteConnection conn = new SQLiteConnection(App.databaseLocation))
-            //{
-            //    conn.CreateTable<Post>();
-
-            //    int affectedRows = conn.Delete(_selectedPost);
-
-            //    if (affectedRows > 0)
-            //        Navigation.PopAsync();
-            //}
-
-            bool result = await Firestore.Delete(_selectedPost);
-            if(result)
-                await Navigation.PopAsync();
-        }
-
-        async void Update_Clicked(System.Object sender, System.EventArgs e)
-        {
-            _selectedPost.Experience = experienceEntry.Text;
-            //using (SQLiteConnection conn = new SQLiteConnection(App.databaseLocation))
-            //{
-            //    conn.CreateTable<Post>();
-
-            //    int affectedRows = conn.Update(_selectedPost);
-
-            //    if (affectedRows > 0)
-            //        Navigation.PopAsync();
-            //}
-
-            bool result = await Firestore.Update(_selectedPost);
-            if(result)
-                await Navigation.PopAsync();
-        }
     }
 }
---
+++ TravelRecordApp/ViewModel/TravelDetailslVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TravelRecordApp.Helpers;
+using Xamarin.Forms;
+
+namespace TravelRecordApp.ViewModel
+{
+    public class TravelDetailslVM
+    {
+        public Command UpdateCommand { get; set; }
+        public Command DeleteCommand { get; set; }
+
+        public TravelDetailslVM()
+        {
+            UpdateCommand = new Command(Update);
+            DeleteCommand = new Command(Delete);
+        }
+
+        private async void Update()
+        {
+            _selectedPost.Experience = experienceEntry.Text;
+
+            bool result = await Firestore.Update(_selectedPost);
+            if (result)
+                await App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private async void Delete()
+        {
+            bool result = await Firestore.Delete(_selectedPost);
+            if (result)
+                await App.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
