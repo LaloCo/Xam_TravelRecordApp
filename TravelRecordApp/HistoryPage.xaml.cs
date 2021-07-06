7297051f063ba0f70183d15ce86f35716ff63da7
Eduardo Rosas <lalorosas@outlook.com>
2021-07-06 11:21:09 -0500

    Work with ObservableCollections

--- TravelRecordApp/HistoryPage.xaml.cs
+++ TravelRecordApp/HistoryPage.xaml.cs
@@ -3,32 +3,26 @@ using System.Collections.Generic;
 using SQLite;
 using TravelRecordApp.Helpers;
 using TravelRecordApp.Model;
+using TravelRecordApp.ViewModel;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
 {
     public partial class HistoryPage : ContentPage
     {
+        private HistoryVM vm;
         public HistoryPage()
         {
             InitializeComponent();
+
+            vm = Resources["vm"] as HistoryVM;
         }
 
-        protected override async void OnAppearing()
+        protected override void OnAppearing()
         {
             base.OnAppearing();
 
-            //using (SQLiteConnection conn = new SQLiteConnection(App.databaseLocation))
-            //{
-            //    conn.CreateTable<Post>();
-            //    var posts = conn.Table<Post>().ToList();
-
-            //    postListView.ItemsSource = posts;
-            //}
-
-            postListView.ItemsSource = null;
-            var posts = await Firestore.Read();
-            postListView.ItemsSource = posts;
+            vm.GetPosts();
         }
 
         void postListView_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
---
+++ TravelRecordApp/ViewModel/HistoryVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using TravelRecordApp.Helpers;
+using TravelRecordApp.Model;
+
+namespace TravelRecordApp.ViewModel
+{
+    public class HistoryVM
+    {
+        public ObservableCollection<Post> Posts { get; set; }
+
+        public HistoryVM()
+        {
+            Posts = new ObservableCollection<Post>();
+        }
+
+        public async void GetPosts()
+        {
+            Posts.Clear();
+            var posts = await Firestore.Read();
+
+            foreach(var post in posts)
+            {
+                Posts.Add(post);
+            }
+        }
+    }
+}
